67f0fb0d6779eb4d8533759d4f8ddf9e3cd4921d
Sam Odom <samodom@me.com>
2014-10-12 21:30:32 -0500

    Initial Commit

---
+++ UIKitSwagger/UIKitSwagger.h
@@ -0,0 +1,19 @@
+//
+//  UIKitSwagger.h
+//  UIKitSwagger
+//
+//  Created by Sam Odom on 10/12/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitSwagger.
+FOUNDATION_EXPORT double UIKitSwaggerVersionNumber;
+
+//! Project version string for UIKitSwagger.
+FOUNDATION_EXPORT const unsigned char UIKitSwaggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitSwagger/PublicHeader.h>
+
+
