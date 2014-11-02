f7dcbcf28d7dba5ac963528939ec2963d7100fe5
Sam Odom <samodom@me.com>
2014-11-01 20:07:04 -0500

    Updated constraint searches to return optional constraint arrays

--- UIKitSwagger/View/UIViewConstraintSearch.swift
+++ UIKitSwagger/View/UIViewConstraintSearch.swift
@@ -10,30 +10,36 @@ import UIKit
 
 public extension UIView {
 
-    public func constraintsForItem(item: AnyObject) -> [Constraint] {
+    public func constraintsForItem(item: AnyObject) -> [Constraint]? {
         return filteredConstraints({ $0.hasItem(item) })
     }
 
-    public func constraintsForAttribute(attribute: NSLayoutAttribute) -> [Constraint] {
+    public func constraintsForAttribute(attribute: NSLayoutAttribute) -> [Constraint]? {
         return filteredConstraints({ $0.hasAttribute(attribute) })
     }
 
-    public func constraintsForItem(item: AnyObject, attribute: NSLayoutAttribute) -> [Constraint] {
+    public func constraintsForItem(item: AnyObject, attribute: NSLayoutAttribute) -> [Constraint]? {
         return filteredConstraints({ $0.hasAttributedItem((item, attribute)) })
     }
 
-    public func constraintsForItems(items: ItemPair) -> [Constraint] {
+    public func constraintsForItems(items: ItemPair) -> [Constraint]? {
         return filteredConstraints({ $0.hasItems(items) })
     }
 
-    public func constraintsForItems(attributedItems: (AttributedItem, AttributedItem)) -> [Constraint] {
+    public func constraintsForItems(attributedItems: (AttributedItem, AttributedItem)) -> [Constraint]? {
         return filteredConstraints({ $0.hasAttributedItems(attributedItems) })
     }
 
     private typealias ConstraintFilter = Constraint -> Bool
 
-    private func filteredConstraints(filter: ConstraintFilter) -> [Constraint] {
+    private func filteredConstraints(filter: ConstraintFilter) -> [Constraint]? {
         let allConstraints = constraints() as [Constraint]
-        return allConstraints.filter(filter)
+        let filteredConstraints = allConstraints.filter(filter)
+        if filteredConstraints.count == 0 {
+            return nil
+        }
+        else {
+            return filteredConstraints
+        }
     }
 }
