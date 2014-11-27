8b500993707a61163d44ccfd67ae2bfccf3a74af
Sam Odom <samodom@me.com>
2014-11-27 17:36:40 -0600

    It was necessary to add a new application target to the project for performing tests where a constraint is applied using a view controller's layout guide

---
+++ TestingDummy/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestingDummy
+//
+//  Created by Sam Odom on 11/27/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ TestingDummy/DummyViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DummyViewController.swift
+//  TestingDummy
+//
+//  Created by Sam Odom on 11/27/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController {
+
+}
+
