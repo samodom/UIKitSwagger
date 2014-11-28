4e49fa540114528eb51e267900b647c995a64391
Sam Odom <samodom@me.com>
2014-11-27 18:14:30 -0600

    Simple test refactors

--- UIKitSwaggerTests/Tests/ConstraintApplicationTests.swift
+++ UIKitSwaggerTests/Tests/ConstraintApplicationTests.swift
@@ -17,7 +17,7 @@ class ConstraintApplicationTests: XCTestCase {
     let child2 = UIView()
     let grandchild1 = UIView()
     let grandchild2 = UIView()
-    let unrelated = UIView()
+    let unrelatedView = UIView()
 
     override func setUp() {
         super.setUp()
@@ -37,57 +37,57 @@ class ConstraintApplicationTests: XCTestCase {
     }
 
     func testApplyingConstantConstraintToView() {
-        let constraint = (child1.height =* 14.42)
+        let constraint = child1.height =* 14.42
         constraint.apply()
-        XCTAssertEqual(child1.constraints().first as Constraint, constraint, "The specified constraint should be added to the view")
+        XCTAssertTrue(contains(child1.constraints() as [Constraint], constraint), "The specified constraint should be added to the view")
     }
 
     func testApplyingReflexiveConstraintToView() {
-        let constraint = (child2.height =* child2.width)
+        let constraint = child2.height =* child2.width
         constraint.apply()
-        XCTAssertEqual(child2.constraints().first as Constraint, constraint, "The specified constraint should be added to the view")
+        XCTAssertTrue(contains(child2.constraints() as [Constraint], constraint), "The specified constraint should be added to the view")
     }
 
     func testApplyingConstraintBetweenParentAndChildViews() {
-        let constraint = (child1.centerX =* view.centerX)
+        let constraint = child1.centerX =* view.centerX
         constraint.apply()
-        XCTAssertEqual(view.constraints().first as Constraint, constraint, "The specified constraint should be added to the parent view")
+        XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "The specified constraint should be added to the parent view")
     }
 
     func testApplyingConstraintBetweenParentAndGrandchildViews() {
-        let constraint = (grandchild1.centerX =* view.centerX)
+        let constraint = grandchild1.centerX =* view.centerX
         constraint.apply()
-        XCTAssertEqual(view.constraints().first as Constraint, constraint, "The specified constraint should be added to the grandparent view")
+        XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "The specified constraint should be added to the grandparent view")
     }
 
     func testAppylyingConstraintBetweenSiblingViews() {
-        let constraint = (child1.left =* child2.right + 15)
+        let constraint = child1.left =* child2.right + 15
         constraint.apply()
-        XCTAssertEqual(view.constraints().first as Constraint, constraint, "The specified constraint should be added to the parent view")
+        XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "The specified constraint should be added to the parent view")
     }
 
     func testApplyingConstraintBetweenCousinViews() {
-        let constraint = (grandchild1.left =* grandchild2.right + 15)
+        let constraint = grandchild1.left =* grandchild2.right + 15
         constraint.apply()
-        XCTAssertEqual(view.constraints().first as Constraint, constraint, "The specified constraint should be added to the parent view")
+        XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "The specified constraint should be added to the parent view")
     }
 
     func testNotApplyingConstraintBetweenUnrelatedViews() {
-        let constraint = (child1.centerX =* unrelated.left)
+        let constraint = child1.centerX =* unrelatedView.left
         constraint.apply()
-        XCTAssertEqual(view.constraints().count, 0, "The specified constraint should not be added to any view")
-        XCTAssertEqual(child1.constraints().count, 0, "The specified constraint should not be added to any view")
-        XCTAssertEqual(unrelated.constraints().count, 0, "The specified constraint should not be added to any view")
+        XCTAssertFalse(contains(view.constraints() as [Constraint], constraint), "The specified constraint should not be added to any view")
+        XCTAssertFalse(contains(child1.constraints() as [Constraint], constraint), "The specified constraint should not be added to any view")
+        XCTAssertFalse(contains(unrelatedView.constraints() as [Constraint], constraint), "The specified constraint should not be added to any view")
     }
 
-    func testCannotApplyConstraintToLayoutSupportFirstItem() {
-        var constraint = (viewController.top =* child2.top - 10)
+    func testApplyingConstraintToLayoutSupportFirstItem() {
+        let constraint = viewController.top =* child2.top - 10
         constraint.apply()
         XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "A constraint involving a view controller guide should be added to the view controller's view")
     }
 
     func testApplyingConstraintToLayoutSupportSecondItem() {
-        var constraint = (child1.top =* viewController.top + 10)
+        let constraint = child1.top =* viewController.top + 10
         constraint.apply()
         XCTAssertTrue(contains(view.constraints() as [Constraint], constraint), "A constraint involving a view controller guide should be added to the view controller's view")
     }
