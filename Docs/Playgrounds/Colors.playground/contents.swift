6463666d8b23a466df29ed62fdd951f3cf6dfc37
Sam Odom <samodom@me.com>
2015-02-26 00:08:08 -0600

    Added color palette to colors playground and made a few other changes there

--- Docs/Playgrounds/Colors.playground/contents.swift
+++ Docs/Playgrounds/Colors.playground/contents.swift
@@ -38,16 +38,12 @@ components = UIColorRGBComponents(red: 0.8, green: 0.6255, blue: 0.342, alpha: 0
 var color = UIColor(components: components)
 
 //: Each component scheme also has a method to produce a color:
-//: * `func color() -> UIColor`
 
 components = UIColorHSBComponents(hue: 0.5, saturation: 0.4, brightness: 0.6, alpha: 0.99)
 color = components.color()
 
 
 //: These component structures can be retrieved from existing colors using the following properties of `UIColor`:
-//: * `var rgbComponents: UIColorRGBComponents`
-//: * `var hsbComponents: UIColorHSBComponents`
-//: * `var grayscaleComponents: UIColorGrayscaleComponents`
 
 components = color.rgbComponents
 components = color.hsbComponents
@@ -57,14 +53,6 @@ components = color.grayscaleComponents
 
 //: ### Component Values
 //: Each component value is now available as a property of `UIColor`
-//: * `var red: CGFloat`
-//: * `var green: CGFloat`
-//: * `var blue: CGFloat`
-//: * `var alpha: CGFloat`
-//: * `var hue: CGFloat`
-//: * `var saturation: CGFloat`
-//: * `var brightness: CGFloat`
-//: * `var white: CGFloat`
 
 color.red
 color.green
@@ -94,3 +82,26 @@ Orange
 Purple
 Brown
 Clear
+
+
+//: ## Color Palette
+//: This class provides a simple interface for storing and retrieving commonly used colors by name.  You can create an empty palette or provide a dictionary of colors.
+//: * `var numberOfColors: Int`
+//: * `func addColor(color: UIColor, named: String)`
+//: * `func colorNamed(name: String) -> UIColor?`
+//: * `func removeColorNamed(name: String)`
+//: * `func removeAllColors()`
+//: Subscripting is also available for added convenience.
+
+var palette = ColorPalette(colors: ["badgeBackground": Orange])
+palette.addColor(Red, named: "warningText")
+palette.addColor(Blue, named: "linkText")
+palette.numberOfColors
+
+let badgeBackgroundColor = palette["badgeBackground"]
+palette["warningText"] = nil
+palette.numberOfColors
+
+palette.removeAllColors()
+palette.numberOfColors
+
