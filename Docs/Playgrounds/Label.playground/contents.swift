ea6c9a0202396563fc43da35b810b606fc1b7198
Sam Odom <samodom@me.com>
2015-02-27 00:12:13 -0600

    Added scratch label playground

---
+++ Docs/Playgrounds/Label.playground/contents.swift
@@ -0,0 +1,13 @@
+import UIKitSwagger
+
+let frame = CGRect(x: 0, y: 0, width: 300, height: 40)
+let label = UILabel(frame: frame)
+label.text = "Sample Label"
+label.textColor = Orange
+
+label
+
+label.disable()
+
+label.enable()
+
