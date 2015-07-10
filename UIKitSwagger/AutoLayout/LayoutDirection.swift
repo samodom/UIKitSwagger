816d95f58c151efc0b87564988e6823c3da8636c
Sam Odom <samodom@me.com>
2015-07-10 12:10:46 -0600

    Added LayoutDirection enumerated type

---
+++ UIKitSwagger/AutoLayout/LayoutDirection.swift
@@ -0,0 +1,41 @@
+//
+//  LayoutDirection.swift
+//  UIKitSwagger
+//
+//  Created by Sam Odom on 7/10/15.
+//  Copyright © 2015 Swagger Soft. All rights reserved.
+//
+
+import UIKit
+
+internal enum LayoutDirection: Int {
+    case LeftToRight = 1
+    case RightToLeft = -1
+    case LeadingToTrailing = 2
+    case TrailingToLeading = -2
+    case TopToBottom = 3
+    case BottomToTop = -3
+
+    internal var attributePair: DirectedAttributePair {
+        let reversed = rawValue < 0
+        let defaultDirection = LayoutDirection(rawValue: abs(rawValue))!
+        let defaultPair = DirectedAttributePairMapping[defaultDirection]!
+        return reversed ? reverseAttributePair(defaultPair) : defaultPair
+    }
+}
+
+internal prefix func -(direction: LayoutDirection) -> LayoutDirection {
+    return LayoutDirection(rawValue: -direction.rawValue)!
+}
+
+internal typealias DirectedAttributePair = (NSLayoutAttribute, NSLayoutAttribute)
+
+private let DirectedAttributePairMapping: [LayoutDirection:DirectedAttributePair] = [
+    .LeftToRight: (.Left, .Right),
+    .LeadingToTrailing: (.Leading, .Trailing),
+    .TopToBottom: (.Top, .Bottom)
+]
+
+private func reverseAttributePair(attributePair: DirectedAttributePair) -> DirectedAttributePair {
+    return (attributePair.1, attributePair.0)
+}
---
+++ UIKitSwaggerTests/Tests/LayoutDirectionTests.swift
@@ -0,0 +1,91 @@
+//
+//  LayoutDirectionTests.swift
+//  UIKitSwagger
+//
+//  Created by Sam Odom on 7/10/15.
+//  Copyright © 2015 Swagger Soft. All rights reserved.
+//
+
+import XCTest
+@testable import UIKitSwagger
+
+class LayoutDirectionTests: XCTestCase {
+
+    var direction: LayoutDirection!
+    var opposite: LayoutDirection!
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testCreatingDirectionFromRawValues() {
+        for raw in -10 ... 10 {
+            direction = LayoutDirection(rawValue: raw)
+            if (1 ... 3).contains(abs(raw)) {
+                XCTAssertTrue(direction != nil, "Raw integer value \(raw) should produce a layout direction")
+            }
+            else {
+                XCTAssertTrue(direction == nil, "The value \(raw) should not be a raw value for a layout direction")
+            }
+        }
+    }
+
+    func testLeftToRightDirection() {
+        direction = .LeftToRight
+        opposite = .RightToLeft
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Left-to-right should use the opposite value of right-to-left")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Left, "The attribute pair should be left followed by right")
+        XCTAssertEqual(direction.attributePair.1, .Right, "The attribute pair should be left followed by right")
+    }
+
+    func testRightToLeftDirection() {
+        direction = .RightToLeft
+        opposite = .LeftToRight
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Right-to-left should use the opposite value of left-to-right")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Right, "The attribute pair should be right followed by left")
+        XCTAssertEqual(direction.attributePair.1, .Left, "The attribute pair should be right followed by left")
+    }
+
+    func testLeadingToTrailingDirection() {
+        direction = .LeadingToTrailing
+        opposite = .TrailingToLeading
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Leading-to-trailing should use the opposite value of trailing-to-leading")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Leading, "The attribute pair should be leading followed by trailing")
+        XCTAssertEqual(direction.attributePair.1, .Trailing, "The attribute pair should be leading followed by trailing")
+    }
+
+    func testTrailingToLeadingDirection() {
+        direction = .TrailingToLeading
+        opposite = .LeadingToTrailing
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Trailing-to-leading should use the opposite value of leading-to-trailing")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Trailing, "The attribute pair should be trailing followed by leading")
+        XCTAssertEqual(direction.attributePair.1, .Leading, "The attribute pair should be trailing followed by leading")
+    }
+
+    func testTopToBottomDirection() {
+        direction = .TopToBottom
+        opposite = .BottomToTop
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Top-to-bottom should use the opposite value of bottom-to-top")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Top, "The attribute pair should be top followed by bottom")
+        XCTAssertEqual(direction.attributePair.1, .Bottom, "The attribute pair should be top followed by bottom")
+    }
+
+    func testBottomToTopDirection() {
+        direction = .BottomToTop
+        opposite = .TopToBottom
+        XCTAssertEqual(direction.rawValue, -opposite.rawValue, "Bottom-to-top should use the opposite value of top-to-bottom")
+        XCTAssertEqual(direction, -opposite, "Negation should produce the reverse direction")
+        XCTAssertEqual(direction.attributePair.0, .Bottom, "The attribute pair should be bottom followed by top")
+        XCTAssertEqual(direction.attributePair.1, .Top, "The attribute pair should be bottom followed by top")
+    }
+
+}
