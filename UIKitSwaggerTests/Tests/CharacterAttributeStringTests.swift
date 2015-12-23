17a7f298167797d3de957b05340cba371addcd18
Sam Odom <samodom@me.com>
2015-12-23 11:42:03 -0700

    Test cleanup

--- UIKitSwaggerTests/Tests/CharacterAttributeStringTests.swift
+++ UIKitSwaggerTests/Tests/CharacterAttributeStringTests.swift
@@ -15,55 +15,68 @@ class CharacterAttributeStringTests: XCTestCase {
     let text = "Sample"
     let font = UIFont.systemFontOfSize(42)
     let offset = CGFloat(14.42)
+    var foregroundColorAttribute: CharacterAttribute!
+    var fontAttribute: CharacterAttribute!
+    var baselineOffsetAttribute: CharacterAttribute!
 
-    var characterAttributeSet: CharacterAttributeSet!
-    var characterAttributeArray: [CharacterAttribute]!
     var attributeDictionary: CharacterAttributeDictionary!
 
     var attributedString: NSAttributedString!
     var expectedString: NSAttributedString!
 
     override func setUp() {
-        characterAttributeArray = [
-            .Font(font),
-            .ForegroundColor(Orange),
-            .BaselineOffset(offset)
-        ]
-        characterAttributeSet = Set(characterAttributeArray)
+        foregroundColorAttribute = .ForegroundColor(Orange)
+        fontAttribute = .Font(font)
+        baselineOffsetAttribute = .BaselineOffset(offset)
 
         attributeDictionary = [
-            NSFontAttributeName: font,
             NSForegroundColorAttributeName: Orange,
+            NSFontAttributeName: font,
             NSBaselineOffsetAttributeName: offset
         ]
+    }
+
+    private func createFullyAttributedString() -> NSAttributedString {
+        return NSAttributedString(string: text, attributes: attributeDictionary)
+    }
+
+    private func createPartiallyAttributedString() -> NSAttributedString {
+        let endRange = NSRange(location: 3, length: characterCount - 3)
+        let mutableString = NSMutableAttributedString(string: text)
+        mutableString.addAttributes(attributeDictionary, range: endRange)
+        return NSAttributedString(attributedString: mutableString)
     }
 
     //  MARK: - Immutable
 
     func testCreatingAttributedStringWithSetOfAttributes() {
-        expectedString = NSAttributedString(string: text, attributes: attributeDictionary)
-        attributedString = NSAttributedString(string: text, characterAttributes: characterAttributeSet)
+        expectedString = createFullyAttributedString()
+        let attributes: CharacterAttributeSet = [
+            foregroundColorAttribute,
+            fontAttribute,
+            baselineOffsetAttribute
+        ]
+        attributedString = NSAttributedString(string: text, characterAttributes: attributes)
         XCTAssertEqual(attributedString, expectedString, "Should be able to create an attributed string using a set of character attributes")
     }
 
     func testCreatingAttributedStringWithArrayOfAttributes() {
-        expectedString = NSAttributedString(string: text, attributes: attributeDictionary)
-        attributedString = NSAttributedString(string: text, characterAttributes: characterAttributeArray)
+        expectedString = createFullyAttributedString()
+        let attributes: [CharacterAttribute] = [
+            foregroundColorAttribute,
+            fontAttribute,
+            baselineOffsetAttribute
+        ]
+        attributedString = NSAttributedString(string: text, characterAttributes: attributes)
         XCTAssertEqual(attributedString, expectedString, "Should be able to create an attributed string using an array of character attributes")
     }
 
     //  MARK: Retrieving single attribute
 
     func testRetrievingCharacterAttributeAtIndex() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         if let attribute = attributedString.characterAttributeAtIndex(3, named: NSForegroundColorAttributeName) {
-            switch attribute {
-            case .ForegroundColor(let color):
-                XCTAssertEqual(color, Orange, "The appropriate attribute should be returned with the correct associated value")
-
-            default:
-                XCTFail("The correct attribute should be returned")
-            }
+            XCTAssertEqual(attribute, foregroundColorAttribute, "The appropriate attribute should be returned with the correct associated value")
         }
         else {
             XCTFail("An attribute should be returned for a valid index and name")
@@ -71,21 +84,15 @@ class CharacterAttributeStringTests: XCTestCase {
     }
 
     func testRetrievingNonexistentCharacterAttributeAtIndex() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         let attribute = attributedString.characterAttributeAtIndex(3, named: NSLigatureAttributeName)
         XCTAssertNil(attribute, "No character attribute should be returned")
     }
 
     func testRetrievingCharacterAttributeAtIndexThroughSubscripting() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         if let attribute = attributedString[3, named: NSForegroundColorAttributeName] {
-            switch attribute {
-            case .ForegroundColor(let color):
-                XCTAssertEqual(color, Orange, "The appropriate attribute should be returned with the correct associated value")
-
-            default:
-                XCTFail("The correct attribute should be returned")
-            }
+            XCTAssertEqual(attribute, foregroundColorAttribute, "The appropriate attribute should be returned with the correct associated value")
         }
         else {
             XCTFail("An attribute should be returned for a valid index and name")
@@ -93,7 +100,7 @@ class CharacterAttributeStringTests: XCTestCase {
     }
 
     func testRetrievingNonexistentCharacterAttributeAtIndexThroughSubscripting() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         let attribute = attributedString[3, named: NSLigatureAttributeName]
         XCTAssertNil(attribute, "No character attribute should be returned")
     }
@@ -101,35 +108,31 @@ class CharacterAttributeStringTests: XCTestCase {
     //  MARK: Retrieving multiple attributes
 
     func testRetrievingSetOfCharacterAttributesAtIndex() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         let attributes = attributedString.characterAttributesAtIndex(3)
         XCTAssertEqual(attributes.count, 3, "There should be three attributes returned")
-        XCTAssert(attributes.contains(.Font(font)), "Each character attribute should be returned with the correct associated value")
-        XCTAssert(attributes.contains(.ForegroundColor(Orange)), "Each character attribute should be returned with the correct associated value")
-        XCTAssert(attributes.contains(.BaselineOffset(offset)), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(foregroundColorAttribute), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(fontAttribute), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(baselineOffsetAttribute), "Each character attribute should be returned with the correct associated value")
     }
 
     func testRetrievingNonexistentCharacterAttributesAtIndex() {
-        let mutableString = NSMutableAttributedString(string: text)
-        mutableString.addAttributes(attributeDictionary, range: NSRange(location: 3, length: 3))
-        attributedString = NSAttributedString(attributedString: mutableString)
+        attributedString = createPartiallyAttributedString()
         let attributes = attributedString.characterAttributesAtIndex(0)
         XCTAssertEqual(attributes.count, 0, "No character attributes should be returned")
     }
 
     func testRetrievingSetOfCharacterAttributesAtIndexThroughSubscripting() {
-        attributedString = NSAttributedString(string: text, attributes: attributeDictionary)
+        attributedString = createFullyAttributedString()
         let attributes = attributedString[3]
         XCTAssertEqual(attributes.count, 3, "There should be three attributes returned")
-        XCTAssert(attributes.contains(.Font(font)), "Each character attribute should be returned with the correct associated value")
-        XCTAssert(attributes.contains(.ForegroundColor(Orange)), "Each character attribute should be returned with the correct associated value")
-        XCTAssert(attributes.contains(.BaselineOffset(offset)), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(foregroundColorAttribute), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(fontAttribute), "Each character attribute should be returned with the correct associated value")
+        XCTAssert(attributes.contains(baselineOffsetAttribute), "Each character attribute should be returned with the correct associated value")
     }
 
     func testRetrievingNonexistentCharacterAttributesAtIndexThroughSubscripting() {
-        let mutableString = NSMutableAttributedString(string: text)
-        mutableString.addAttributes(attributeDictionary, range: NSRange(location: 3, length: 3))
-        attributedString = NSAttributedString(attributedString: mutableString)
+        attributedString = createPartiallyAttributedString()
         let attributes = attributedString[0]
         XCTAssertEqual(attributes.count, 0, "No character attributes should be returned")
     }
