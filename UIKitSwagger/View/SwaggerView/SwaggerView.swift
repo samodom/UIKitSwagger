d94d4a24402040936eb4b6dd12b3e9b2825e531a
Sam Odom <samodom@icloud.com>
2016-04-25 20:55:10 -0600

    Introduced Swagger views

---
+++ UIKitSwagger/View/SwaggerView/SwaggerView.swift
@@ -0,0 +1,48 @@
+//
+//  SwaggerView.swift
+//  UIKitSwagger
+//
+//  Created by Sam Odom on 4/25/16.
+//  Copyright © 2016 Swagger Soft. All rights reserved.
+//
+
+import UIKit
+
+public class SwaggerView: UIView {
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        performCommonInitialization()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        performCommonInitialization()
+    }
+
+    private func performCommonInitialization() {
+        backgroundColor = nil
+    }
+
+    override public class func layerClass() -> AnyClass {
+        return CAShapeLayer.self
+    }
+
+    private var shapeLayer: CAShapeLayer {
+        return layer as! CAShapeLayer
+    }
+
+    override public var backgroundColor: UIColor? {
+        get {
+            guard let color = shapeLayer.fillColor else {
+                return nil
+            }
+
+            return UIColor(CGColor: color)
+        }
+        set {
+            shapeLayer.fillColor = newValue?.CGColor
+        }
+    }
+
+}
